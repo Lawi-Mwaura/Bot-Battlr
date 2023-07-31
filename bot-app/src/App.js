8a14577f429f073625cab7377b6a8e8d7ba291f3
Lawi-Mwaura <Lawimwaura@gmail.com>
2023-07-31 05:23:33 +0300

    Addedcode to component 'YourBotArmy'

--- bot-app/src/App.js
+++ bot-app/src/App.js
@@ -1,36 +1,47 @@
-import React, {UseState, useEffect} from 'react';// Importing hooks from react library
-import YourBotArmy from './YourBotArmy';// Importing the YourBotArmy component
+import React, { useState, useEffect} from 'react';// Importing hooks from react library
+import YourBotArmy from './Components/YourBotArmy';// Importing the YourBotArmy component
 import SortBar from './SortBar';// Importing the SortBar component
 import FilterBar from './FilterBar';// Importing the FilterBar component
 import BotCollection from './BotCollection';// Importing the BotCollection component
 import './App.css';//Importing the default styles
 
 function App() {
-  const [bots, setBots] = useState([]);// Initial state of the bots
-  const [enlistBots, setEnlistBots] = useState([]);// Initial state of the enlistBots
-  const [releaseBot, setReleaseBot] = useState([]);// Initial state of the releaseBot
-  const [dischargeBot, setDischargeBot] = useState([]);// Initial state of the dischargeBot
+  const [bots, setBots] = useState([]);
+  const [enlistBots, setEnlistedBots] = useState([]);
+  const [releaseBot, setReleaseBot] = useState([]);
+  const [dischargeBot, setDischargeBot] = useState([]);
 
   useEffect(() => {
-    fetch('db.json')
-    .then(res => res.json())// Fetching the db.json
-    .then(data => setBots(data))// Setting the bots
+    fetchBots();
   }, []);
-
+  
   const fetchBots = async () => {
-    const response =await fetch ("https://localhost:8001");
+    const response = await fetch('https://localhost:8001/bots');
     const data = await response.json();
     setBots(data);
-  } catch (error) {
-    console.log(error);
   }
-};
+  };
 
+  const enlistBot = (bot) => {
+    setEnlistedBots((prevBots) => [...prevBots, bot]);
+  };
+ 
   
+  const releaseBot = (bot) => {
+    setEnlistedBots((prevBots) => [...prevBots.filter, bot.id]);//copying the array and filtering the bot according to the id
+  };
 
-
-
+  const dischargeBot = async (bot) => {
+    try {
+      await fetch(`https://localhost:8001/${bot.id}`, {
+        method: 'DELETE',
+      });
+    } catch (error) {
+      console.log(error);
+    };
+  };
   
+  const BotArmy = () => {
   return (
     <div>
       <h1>Bot Army</h1>
@@ -40,6 +51,6 @@ function App() {
       <YourBotArmy bots={enlistBots} release={releaseBot} dischargeBot={dischargeBot} />
     </div>
   );
+  }
 
-
-export default App;
+export default BotArmy;
